0476eb816b66f7f1685b1800b37ec7b6c1180929
IhorKohut <ih.kohut@gmail.com>
2020-02-28 18:35:33 +0200

    Edit file homework

--- Year.java
+++ Year.java
@@ -4,31 +4,37 @@ import java.io.InputStreamReader;
 
 public class Year {
 
-     public static void main(String[] args) throws IOException {
+    public enum Seasons {
+        Winter , Spring, Summer, Autumn;
+    }
+
+    public static void main(String[] args) throws IOException {
      BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+     System.out.println("Input month");
      String s = reader.readLine();
+
 
          switch(s)
              {
              case "February":
              case "January":
              case "December":
-                 System.out.println("Winter");
+                 System.out.println(Seasons.Winter);
                  break;
              case "March":
              case "April":
              case "May":
-                 System.out.println("Spring");
+                 System.out.println(Seasons.Spring);
                  break;
              case "June":
              case "July":
              case "August":
-                 System.out.println("Summer");
+                 System.out.println(Seasons.Summer);
                  break;
              case "September":
              case "October":
              case "November":
-                 System.out.println("Autumn");
+                 System.out.println(Seasons.Autumn);
                  break;
 
                 default: System.out.println("Incorrect");
